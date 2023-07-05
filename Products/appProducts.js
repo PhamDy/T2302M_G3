9b6e20da19243c44ddf2fd7ceea62b4ca6d5c8b4
phamdacdy <phamdacdy@gmail.com>
2023-07-06 01:02:46 +0700

    my Cart

--- Products/appProducts.js
+++ Products/appProducts.js
@@ -29,30 +29,54 @@ angular.module('myApp', [])
 
 
     // Add to cart
+    $scope.totalPrice = 0;
+    
+    // Hàm tính subTotal
+    $scope.calculateSubTotal = function(item) {
+      return item.quantity * item.price;
+    };
+    // Hàm tính tổng giá trị
+    $scope.calculateTotalPrice = function() {
+      $scope.totalPrice = 0;
+
+      for (var i = 0; i < $scope.cartItems.length; i++) {
+        var item = $scope.cartItems[i];
+        if (item.quantity && item.price) {
+          item.subTotal = $scope.calculateSubTotal(item); 
+          $scope.totalPrice += item.subTotal;
+        }
+      }
+    };
+    
+    // Thêm vào giỏ hàng
     $scope.cartItems = [];
+    if (sessionStorage.getItem('cartItems')) {
+      $scope.cartItems = angular.fromJson(sessionStorage.getItem('cartItems'));
+    }
     $scope.count = 0;
+    if (sessionStorage.getItem('count')) {
+      $scope.count = angular.fromJson(sessionStorage.getItem('count'));
+    }
     $scope.addToCart = function(product) {
       var cart = {
           "id": product.id,
           "name": product.name,
           "price": product.price,
-          "img": product.img1
-      }
-      // Chuyển đổi đối tượng thành chuỗi JSON và lưu trữ vào localStorage
-      $window.localStorage.setItem('cart', JSON.stringify(cart));
-
-      // Lấy chuỗi JSON từ localStorage và chuyển đổi thành đối tượng
-      var storedData = $window.localStorage.getItem('cart');
-      var cartItems = JSON.parse(storedData);
-
-      console.log(cartItems); 
-
-      if (cartItems !== null) {
-        $scope.count++;
+          "img": product.img1,
+          "quantity": 1
+      };
+      if (cart !== null) {
         $scope.cartItems.push(cart);
-      } 
-      
+        $scope.count = $scope.cartItems.length;
+      }       
+      sessionStorage.setItem('cartItems',angular.toJson($scope.cartItems))
+      sessionStorage.setItem('count',angular.toJson($scope.count))
+      console.log($scope.cartItems)
+      $scope.calculateTotalPrice();
+      console.log($scope.totalPrice);
     }
+    
+    // Show cart
     $scope.showCart = false;
     $scope.showcart = function() {
       $scope.showCart = !$scope.showCart 
@@ -61,18 +85,25 @@ angular.module('myApp', [])
     $scope.closeCart = function() {
       $scope.showCart = false; 
     };
-
+    
+    // Delete product
     $scope.delete = function(id) {
-      // console.log("ID:", id);
       for (var i = 0; i < $scope.cartItems.length; i++) {
         if ($scope.cartItems[i].id === id) {
           $scope.cartItems.splice(i, 1);
           break; 
         }
       }
+      // Cập nhật số lượng
+    $scope.count = $scope.cartItems.length;
+     $window.localStorage.setItem('cart', JSON.stringify($scope.cartItems));
+     $scope.calculateTotalPrice();
+     console.log($scope.totalPrice);
     };
+    $scope.updateQuantity = function() {
+      $scope.calculateTotalPrice();
+    };
+
     
-
    
-
   });
