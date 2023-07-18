a047465436a626c38ef8230849f323d6782d6992
nguyenduyhoang03 <hoangtop2003@gmail.com>
2023-07-18 23:26:14 +0700

    sửa lại login

--- js/login.js
+++ js/login.js
@@ -1,168 +1,121 @@
-// register form
-const form = document.getElementById('register-form')
-const username = document.getElementById('username')
-const email = document.getElementById('email')
-const password = document.getElementById('password')
-const password2 = document.getElementById('password2')
+var registerApp = angular.module('registerApp', []);
 
-// Show input error message
-function showError(input, message) {
-	const formControl = input.parentElement
-	formControl.className = 'form-control error'
-	const small = formControl.querySelector('small')
-	small.innerText = message
+registerApp.controller('registerCtrl', function($scope) {
+
+$scope.passwordsMatch = true; // Initialize variable to store password comparison results
+
+$scope.checkPasswordMatch = function() {
+  $scope.passwordsMatch = $scope.regForm.password.$viewValue === $scope.regForm.confirmPassword.$viewValue;
+};
+if (!localStorage.getItem('users')) {
+  localStorage.setItem('users', JSON.stringify([]));
+}
+var users = JSON.parse(localStorage.getItem('users'));
+
+// Kiểm tra xem tài khoản admin đã tồn tại trong danh sách người dùng hay chưa
+var adminExists = users.some(function(user) {
+  return user.username === 'admin';
+});
+
+// Nếu tài khoản admin chưa tồn tại, thêm nó vào danh sách người dùng
+if (!adminExists) {
+  var admin = {
+    username: 'admin',
+    password: '123456',
+  };
+  users.push(admin);
+  localStorage.setItem('users', JSON.stringify(users));
 }
 
-// Show success outline
-function showSuccess(input,message) {
-	const formControl = input.parentElement
-	formControl.className = 'form-control success'
-    const small = formControl.querySelector('small')
-	small.innerText = message
-}
+// The function stores registration information to local storage
+$scope.saveUserInfoToLocalStorage = function(username, email, password) {
+  var users = JSON.parse(localStorage.getItem("users"));
 
-// Check email is valid
-// kiểu nó phải đúng @gmail ý
-function checkEmail(input) {
-	const re =
-		/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-
-	if (re.test(input.value.trim())) {
-		showSuccess(input,'')
-	} else {
-		showError(input, 'Email is not valid')
-	}
-}
-// get field input name
-  function getFieldName(input) {
-	return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+   if (!users) { // Kiểm tra nếu users là null hoặc không tồn tại
+    users = []; // Khởi tạo users như một mảng rỗng
   }
-// check required
-function checkRequired(inputArr) {
-	let isRequired = false;
-	inputArr.forEach(function (input) {
-	  if (input.value.trim() === '') {
-		showError(input, `${getFieldName(input)} is required`);
-		isRequired = true;
-	  } else {
-		showSuccess(input,'');
-	  }
-	});
-  
-	return isRequired;
+  users.push({ username: username, email: email, password: password });
+  localStorage.setItem("users", JSON.stringify(users));
+};
+
+//checks if the username already exists or not
+$scope.checkIfUsernameExists = function(username) {
+  var users = JSON.parse(localStorage.getItem("users"));
+  var usernameExists = false;
+
+  if (users) { // Kiểm tra giá trị users trước khi sử dụng forEach
+    users.forEach(function(user) {
+      if (user.username === username) {
+        usernameExists = true;
+        return; // dừng vòng lặp nếu username đã tồn tại
+      }
+    });
   }
 
-  
+  return usernameExists;
+};
 
-// Check input length
-function checkLength(input, min, max) {
-	if (input.value.length < min) {
-		showError(
-			input,
-			`${getFieldName(input)} must be at least ${min} characters`
-		)
-	} else if (input.value.length > max) {
-		showError(
-			input,
-			`${getFieldName(input)} must be less than ${max} characters`
-		)
-	} else {
-		showSuccess(input,'')
-	}
-}
 
-// Check passwords match
-function checkPasswordsMatch(input1, input2) {
-	if (input1.value !== input2.value) {
-		showError(input2, 'Passwords do not match')
-	}
-}
-//  add account
-function registerUser(username, email, password) {
-	const user = {
-	  username,
-	  email,
-	  password
-	};
-	
-	// Kiểm tra xem đã có dữ liệu trong local storage hay chưa
-	let registeredUsers = localStorage.getItem('registeredUsers');
-	if (!registeredUsers) {
-	  // Nếu chưa có dữ liệu, tạo một mảng mới
-	  registeredUsers = [];
-	} else {
-	  // Nếu đã có dữ liệu, phân tích cú pháp dữ liệu JSON
-	  registeredUsers = JSON.parse(registeredUsers);
-	}
-	
-	// Thêm tài khoản mới vào mảng
-	registeredUsers.push(user);
-	
-	// Lưu trữ mảng vào local storage
-	localStorage.setItem('registeredUsers', JSON.stringify(registeredUsers));
+
+// when user click register
+$scope.register = function() {
+  // check username is exist or not
+  if ($scope.checkIfUsernameExists($scope.username)) {
+    // Username already exists, so registration information cannot be stored in local storage
+    alert('username already exist !');
+  } else {
+    //Username does not exist yet, so registration information can be stored in local storage
+    $scope.saveUserInfoToLocalStorage($scope.username, $scope.email, $scope.password);
+    alert('Register Success !');
+    regForm.reset()
   }
-  
+};
+// login app
+ $scope.login = function(e) {
+      e.preventDefault();
 
-// Event listeners
-form.addEventListener('submit', function (e) {
-	e.preventDefault()
+      var users = JSON.parse(localStorage.getItem("users"));
+      var username = $scope.username;
+      var password = $scope.password;
+      var validLogin = false;
 
-	if (!checkRequired([username, email, password, password2])) {
-		checkLength(username, 3, 25)
-		checkLength(password, 6, 25)
-		checkEmail(email)
-		checkPasswordsMatch(password, password2)
-	}
-	alert('register success!')
-    registerUser(username.value, email.value, password.value);
-	form.reset();
+      for (var i = 0; i < users.length; i++) {
+        if (users[i].username === username && users[i].password === password) {
+          validLogin = true;
+          break; // Dừng vòng lặp khi tìm thấy đăng nhập hợp lệ
+        }
+      }
+
+      if (validLogin) {
+        if (username === 'admin' && password === '123456') {
+          // Đăng nhập thành công với tài khoản admin
+          alert('Login Successful as Admin!');
+          $scope.logForm.$setPristine(); // Đặt lại form về trạng thái ban đầu
+          location.href = '/T2302M_G3/admin/admin.html'; // Chuyển hướng đến trang "admin.html"
+        } else {
+          // Đăng nhập thành công với các tài khoản khác
+          alert('Login Successful!');
+          $scope.logForm.$setPristine(); // Đặt lại form về trạng thái ban đầu
+          location.href = '/T2302M_G3/src/index.html'; // Chuyển hướng đến trang "1.html"
+        }
+      } else {
+        alert('Invalid username or password!');
+      }
+      
+
+
+    };
+    
 });
 
 
 
-// login form
-const login_Form = document.getElementById('login-form');
-const user_Name = document.getElementById('user-name');
-const user_Password = document.getElementById('user-password');
-// Kiểm tra tài khoản admin
-function checkAdmin(username, password) {
-	return username === 'admin' && password === '123456';
-  }
-// check account
-function loginUser(username, password) {
-	const storedUsers = JSON.parse(localStorage.getItem('registeredUsers'));
-  
-	// if (storedUsers) {
-	//   const foundUser = storedUsers.find(user => user.username === username && user.password === password);
-	//   if (foundUser) {
-	// 	showSuccess(user_Password, 'Login Success ! Go to home page in 1 seconds !');
-	// 	setTimeout(function(){
-	// 		location.href = '/T2302M_G3/index.html';
-	// 	},1000);
-	//   } else {
-	// 	showError(user_Password, 'User Name or Password is incorrect !');
-	//   }
-	// }
-	if (checkAdmin(username, password)) {
-		showSuccess(user_Password, 'Login Success as Admin! Redirecting to admin page in 1 second!');
-		setTimeout(function(){
-		  location.href = '/T2302M_G3/admin/admin.html';
-		}, 1000);
-	  } else {
-		showSuccess(user_Password, 'Login Success! Redirecting to home page in 1 second!');
-		setTimeout(function(){
-		  location.href = '/T2302M_G3/index.html';
-		}, 1000);
-	  }
-  }
-  
-// Event listeners
-login_Form.addEventListener('submit',function(e){
-	e.preventDefault();
 
-	loginUser(user_Name.value,user_Password.value);
-})
-  
+
+
+
+
+
 // switch between registration and login
 const registerLink = document.getElementById('register-link');
 const loginLink = document.getElementById('login-link');
@@ -180,22 +133,3 @@ loginLink.addEventListener('click', function (e) {
   registerForm.style.display = 'none';
   loginForm.style.display = 'block';
 });
-
-// Xem danh sách các tài khoản đã lưu
-// function showRegisteredUsers() {
-// 	const registeredUsers = localStorage.getItem('registeredUsers');
-// 	if (registeredUsers) {
-// 	  const users = JSON.parse(registeredUsers);
-// 	  console.log('Danh sách các tài khoản đã đăng ký:');
-// 	  users.forEach((user, index) => {
-// 		console.log(`Tài khoản ${index + 1}:`);
-// 		console.log(`Username: ${user.username}`);
-// 		console.log(`Email: ${user.email}`);
-// 		console.log(`Password: ${user.password}`);
-// 		console.log('---------------------');
-// 	  });
-// 	} else {
-// 	  console.log('Chưa có tài khoản nào được đăng ký');
-// 	}
-//   }
-  
